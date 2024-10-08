8240d0c7d790f46f572574b851046f8847325ff0
Shivam <139581888+shiv325@users.noreply.github.com>
2024-10-08 17:40:15 +0530

    Create Remove Element.cpp

---
+++ 000027. Remove Element/Remove Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector <int> :: iterator it;
+        it = nums.begin();
+        while (it != nums.end()) {
+            if (*it == val) {
+                nums.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+        return nums.size();
+    }
+}
